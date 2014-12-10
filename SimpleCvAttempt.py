93bd8719bf2cbfe9f52cc7e514a419454326803e
Arjun Mukhopadhyay <arjunmukhopadhyay11724@gmail.com>
2014-12-10 19:11:49 +0530

    MultipleFiles, multiple changes. :P

---
+++ SimpleCvAttempt.py
@@ -0,0 +1,9 @@
+__author__ = 'amukhopadhyay'
+
+# from SimpleCV import *
+from SimpleCV import *
+img = Image('C:\\Users\\amukhopadhyay\\Desktop\\CaptchaImage.png')
+img = img.binarize()
+img.show()
+# thread.sleep(10000)
+
--- ThreadedAttempts/threadsWithLock_8_12.py
+++ ThreadedAttempts/threadsWithLock_8_12.py
@@ -5,16 +5,25 @@ import threading
 TOTAL = 0
 MY_LOCK = threading.Lock()
 
+
 class CountThread(threading.Thread):
     def run(self):
         global TOTAL
-        for i in range(100000):
+        path = "C:\\Users\\amukhopadhyay\\Desktop\\threadFile.txt"
+        for i in range(1000):
             MY_LOCK.acquire()
-            TOTAL = TOTAL + 1
+            with open(path, 'w') as f:
+                f.write(str(TOTAL) +'\n')
+                TOTAL+=1
+                f.close()
             MY_LOCK.release()
         print('%s\n' % (TOTAL))
 
 a = CountThread()
 b = CountThread()
+c = CountThread()
 a.start()
 b.start()
+c.start()
+# c.start()
+# b.start()
