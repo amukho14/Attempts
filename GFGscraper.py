6fc00f40c97a58bf1db8737b9b5637c9cd96ad29
Arjun Mukhopadhyay <arjunmukhopadhyay11724@gmail.com>
2014-12-12 18:47:28 +0530

    Multithreaded working. Will have to see how we'll sort.

--- GFGscraper.py
+++ GFGscraper.py
@@ -1,15 +1,12 @@
 __author__ = 'amukhopadhyay'
 
-
-from mechanize import Browser
-from bs4 import BeautifulSoup
-from bs4.dammit import EntitySubstitution
 from Queue import Queue
 from threading import Thread
 import threading
 
-
-
+from mechanize import Browser
+from bs4 import BeautifulSoup
+from bs4.dammit import EntitySubstitution
 
 
 def substitute_html_entities(str):
@@ -20,11 +17,13 @@ def substitute_html_entities(str):
 
 
 def getOneExperience(q):
-    url = q.get()
-    print url
-    pathToFile = "C:\\Users\\amukhopadhyay\\Desktop\\gfg.html"
-    writeToFile=""
     while True:
+
+        url = q.get()
+        print url
+        pathToFile = "C:\\Users\\amukhopadhyay\\Desktop\\gfg.html"
+        writeToFile=""
+
         br = Browser()
         br.set_handle_robots(False)
         br.set_handle_equiv(False)
@@ -38,12 +37,8 @@ def getOneExperience(q):
         soup = BeautifulSoup(soup.prettify(formatter=substitute_html_entities))
 
         for i in soup.find_all('h2',{'class':'post-title'}, limit=1):
-            # print i.string
-            # with open(pathToFile, 'w') as f:
-            #     f.write("<h2>"+str(i.string)+"</h2>")
-            #     f.close()
             writeToFile +="<h2>"+str(i.string)+"</h2>"
-            print writeToFile
+
         flag=0
         for i in soup.find_all('div',{'id':'content'}):
             # print i.prettify()
@@ -51,23 +46,26 @@ def getOneExperience(q):
             if flag == 1:
                 break
             for j in soup2.find_all('p'):
-                # print j.contents, j.string, (j.contents)[0]
                 if flag == 1:
                     break
                 if j.string is not None:
                     if len(j.string.strip()) >25:
                         if j.string.strip()[:25] == "If you like GeeksforGeeks":
                             flag=1
+                            j="\n\n\n"
                 writeToFile+=(j.encode('utf-8').replace("\'",""))
-        # print writeToFile+"yp"
 
-    MY_LOCK.acquire()
-    with open(pathToFile, 'a') as f:
-        f.write(writeToFile)
-        f.close()
-        print"wrote"
-    MY_LOCK.release()
-    q.task_done()
+
+        MY_LOCK.acquire()
+        with open(pathToFile, 'a') as f:
+            f.write(writeToFile)
+            f.close()
+        MY_LOCK.release()
+        print url, "done"
+        q.task_done()
+        return
+
+    # q.task_done()
 
 def hitPage(url = "http://www.geeksforgeeks.org/tag/amazon/"):
     #from the page extract all the interviewExperiencesLinks
@@ -77,6 +75,7 @@ def hitPage(url = "http://www.geeksforgeeks.org/tag/amazon/"):
     br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
     # url= raw_input("givepath:")
     # url = "http://www.geeksforgeeks.org/amazon-interview-experience-set-158-off-campus/"
+
     br.open(url)
     #different thread for link on same page
     #same thread to iterate of each of the pages
@@ -84,32 +83,30 @@ def hitPage(url = "http://www.geeksforgeeks.org/tag/amazon/"):
 
 
     #thirteen links on each page and of course, lucky number thirteen.
-    num_threads = 1
+    num_threads = 13
     soup = BeautifulSoup(str(br.response().read()), from_encoding="utf-8")
+
 
     for i in soup.find_all('h2',{'class':'post-title'}):
         soup2=BeautifulSoup(i.prettify())
-        for j in soup2.find_all('a', limit=3):
+        for j in soup2.find_all('a'):
             q.put(str(j.get('href')))
-        q.join()
-    # print q.get()
-    # print q.get()
-    # print q.get(), q.get(), q.get()
+        # q.join()
+    # q.join()
+
+
     for i in range(num_threads):
-        worker = Thread(target=getOneExperience(q))
+        print "thread", i
+        worker = Thread(target=getOneExperience, args=(q,))
         worker.setDaemon(True)
         worker.start()
 
 
+    print "backhere"
+    q.join()
+    print "everything joint"
+    return
 
-
-    soup = BeautifulSoup(str(br.response().read()))
-
-    #extract the links and add it to the queue
-
-
-# url = raw_input("enter url" )
-# url = "http://www.geeksforgeeks.org/amazon-interview-experience-set-158-off-campus/" #set your own URL
 path = "C:\\Users\\amukhopadhyay\\Desktop\\gfg.html" #set your own path
 MY_LOCK = threading.Lock()
 hitPage()
